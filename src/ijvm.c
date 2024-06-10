9529cec5aca67e73e021244a099833296a4e9ad8
Tsurugi21 <m.bentaher@student.vu.nl>
2024-06-10 18:57:23 +0200

    completed task #4

--- src/ijvm.c
+++ src/ijvm.c
@@ -7,35 +7,29 @@
 
 // see ijvm.h for descriptions of the below functions
 
-void push(ijvm* m, int8_t value) 
-{ 
+void push(ijvm *m, int8_t value)
+{
   m->st->data[m->st->index_top] = value;
   m->st->index_top++;
 
   // Check if stack is full
-  if(m->st->index_top >= m->st->size) {
+  if (m->st->index_top >= m->st->size)
+  {
     m->st->size *= 2;
     m->st->data = (int8_t *)realloc(m->st->data, m->st->size);
   }
 }
 
-int8_t pop(ijvm* m) 
-{ 
-  int8_t return_val = 0;
-  
-  if(m->st->index_top > 0) {
-    m->st->index_top--;
-    return_val = m->st->data[m->st->index_top];
-  }
-
-  return return_val;
-  
+int8_t pop(ijvm *m)
+{
+  m->st->index_top--;
+  return m->st->data[m->st->index_top];
 }
 
-ijvm* init_ijvm(char *binary_path, FILE* input , FILE* output) 
+ijvm *init_ijvm(char *binary_path, FILE *input, FILE *output)
 {
   // do not change these first three lines
-  ijvm* m = (ijvm *) malloc(sizeof(ijvm));
+  ijvm *m = (ijvm *)malloc(sizeof(ijvm));
   // note that malloc gives you memory, but gives no guarantees on the initial
   // values of that memory. It might be all zeroes, or be random data.
   // It is hence important that you initialize all variables in the ijvm
@@ -51,7 +45,8 @@ ijvm* init_ijvm(char *binary_path, FILE* input , FILE* output)
   fread(&buffer, sizeof(uint8_t), 4, fp);
   m->magic_num = read_uint32(buffer);
   // Check the magic number
-  if(m->magic_num != MAGIC_NUMBER) return NULL;
+  if (m->magic_num != MAGIC_NUMBER)
+    return NULL;
 
   // Read the constant origin
   fread(&buffer, sizeof(uint8_t), 4, fp);
@@ -60,13 +55,13 @@ ijvm* init_ijvm(char *binary_path, FILE* input , FILE* output)
   fread(&buffer, sizeof(uint8_t), 4, fp);
   m->constant_size = read_uint32(buffer);
   // Read the constant data
-  m->constant_data = (word_t *)malloc(sizeof(word_t) * m->constant_size/4);
-  for (uint32_t i = 0; i < m->constant_size/4; i++)
+  m->constant_data = (word_t *)malloc(sizeof(word_t) * m->constant_size / 4);
+  for (uint32_t i = 0; i < m->constant_size / 4; i++)
   {
     fread(&buffer, sizeof(uint8_t), 4, fp);
     m->constant_data[i] = read_uint32(buffer);
   }
-  
+
   // Read the text pool
   uint8_t buffer_small;
   fread(&buffer, sizeof(uint8_t), 4, fp);
@@ -98,237 +93,269 @@ ijvm* init_ijvm(char *binary_path, FILE* input , FILE* output)
   {
     push(m, i);
   }
-  
 
   return m;
 }
 
-void destroy_ijvm(ijvm* m) 
+void destroy_ijvm(ijvm *m)
 {
   free(m); // free memory for struct
 }
 
-byte_t *get_text(ijvm* m) 
+byte_t *get_text(ijvm *m)
 {
   return m->text_data;
 }
 
-unsigned int get_text_size(ijvm* m) 
+unsigned int get_text_size(ijvm *m)
 {
   return m->text_size;
 }
 
-word_t get_constant(ijvm* m,int i) 
+word_t get_constant(ijvm *m, int i)
 {
   return m->constant_data[i];
 }
 
-unsigned int get_program_counter(ijvm* m) 
+unsigned int get_program_counter(ijvm *m)
 {
   return m->pc;
 }
 
-word_t tos(ijvm* m) 
+word_t tos(ijvm *m)
 {
-  assert(m->st->index_top > 0);
-  return m->st->data[m->st->index_top-1];
+  return m->st->data[m->st->index_top - 1];
 }
 
-bool finished(ijvm* m) 
+bool finished(ijvm *m)
 {
   return m->is_finished;
 }
 
-word_t get_local_variable(ijvm* m, int i) 
+word_t get_local_variable(ijvm *m, int i)
 {
   word_t local_var = m->st->data[i];
   return local_var;
 }
 
-int16_t get_short_arg(ijvm* m) {
-  uint8_t short_bytes[] = {get_text(m)[m->pc+1], get_text(m)[m->pc+2]};
+int16_t get_short_arg(ijvm *m)
+{
+  uint8_t short_bytes[] = {get_text(m)[m->pc + 1], get_text(m)[m->pc + 2]};
   return read_int16(short_bytes);
 }
 
-void step(ijvm* m) 
+void step(ijvm *m)
 {
-  if (get_program_counter(m) + 1 >= get_text_size(m)) {
+  if (get_program_counter(m) + 1 >= get_text_size(m))
+  {
     m->is_finished = true;
     return;
   }
 
   byte_t instruction = get_instruction(m);
 
-  switch(instruction) {
-    case OP_BIPUSH: {
-      m->pc++;
-      push(m, get_instruction(m));
-      break;
+  switch (instruction)
+  {
+  case OP_BIPUSH:
+  {
+    m->pc++;
+    push(m, get_instruction(m));
+    break;
+  }
+  case OP_DUP:
+  {
+    push(m, tos(m));
+    break;
+  }
+  case OP_IADD:
+  {
+    push(m, pop(m) + pop(m));
+    break;
+  }
+  case OP_IAND:
+  {
+    push(m, pop(m) & pop(m));
+    break;
+  }
+  case OP_IOR:
+  {
+    push(m, pop(m) | pop(m));
+    break;
+  }
+  case OP_ISUB:
+  {
+    int8_t val1 = pop(m);
+    int8_t val2 = pop(m);
+    push(m, val2 - val1);
+    break;
+  }
+  case OP_NOP:
+  {
+    break;
+  }
+  case OP_POP:
+  {
+    pop(m);
+    break;
+  }
+  case OP_SWAP:
+  {
+    int8_t val1 = pop(m);
+    int8_t val2 = pop(m);
+    push(m, val1);
+    push(m, val2);
+    break;
+  }
+  case OP_ERR:
+  {
+    fprintf(m->out, "ERROR\n");
+    m->is_finished = true;
+    break;
+  }
+  case OP_HALT:
+  {
+    m->is_finished = true;
+    break;
+  }
+  case OP_IN:
+  {
+    push(m, fgetc(m->in));
+    break;
+  }
+  case OP_OUT:
+  {
+    fprintf(m->out, "%c", (char)pop(m));
+    break;
+  }
+  case OP_GOTO:
+  {
+    m->pc += get_short_arg(m) - 1;
+    break;
+  }
+  case OP_IFEQ:
+  {
+    if (pop(m) == 0)
+    {
+      m->pc += get_short_arg(m) - 1;
     }
-    case OP_DUP: {
-      push(m, tos(m));
-      break;
-    }
-    case OP_IADD: {
-      push(m, pop(m) + pop(m));
-      break;
-    }
-    case OP_IAND: {
-      push(m, pop(m) & pop(m));
-      break;
-    }
-    case OP_IOR: {
-      push(m, pop(m) | pop(m));
-      break;
-    }
-    case OP_ISUB: {
-      int8_t val1 = pop(m);
-      int8_t val2 = pop(m);
-      push(m, val2 - val1);
-      break;
-    }
-    case OP_NOP: {
-      break;
-    }
-    case OP_POP: {
-      pop(m);
-      break;
-    }
-    case OP_SWAP: {
-      int8_t val1 = pop(m);
-      int8_t val2 = pop(m);
-      push(m, val1);
-      push(m, val2);
-      break;
-    }
-    case OP_ERR: {
-      fprintf(m->out, "ERROR\n");
-      m->is_finished = true;
-      break;
-    }
-    case OP_HALT: {
-      m->is_finished = true;
-      break;
-    }
-    case OP_IN: {
-      push(m, fgetc(m->in));
-      break;
-    }
-    case OP_OUT: {
-      fprintf(m->out, "%c", (char)pop(m));
-      break;
-    }
-    case OP_GOTO: {
-      m->pc += get_short_arg(m)-1;
-      break;
-    }
-    case OP_IFEQ: {
-      if(pop(m) == 0){
-        m->pc += get_short_arg(m)-1;
-      } else {
-        m->pc += 2;
-      }
-      break;
-    }
-    case OP_IFLT: {
-      if(pop(m) < 0){
-        m->pc += get_short_arg(m)-1;
-      } else {
-        m->pc += 2;
-      }
-      break;
-    }
-    case OP_IF_ICMPEQ: {
-      if(pop(m) == pop(m)){
-        m->pc += get_short_arg(m)-1;
-      } else {
-        m->pc += 2;
-      }
-      break;
-    }
-    case OP_LDC_W: {
-      push(m, get_constant(m, get_short_arg(m)));
+    else
+    {
       m->pc += 2;
+    }
+    break;
+  }
+  case OP_IFLT:
+  {
+    if (pop(m) < 0)
+    {
+      m->pc += get_short_arg(m) - 1;
+    }
+    else
+    {
+      m->pc += 2;
+    }
+    break;
+  }
+  case OP_IF_ICMPEQ:
+  {
+    if (pop(m) == pop(m))
+    {
+      m->pc += get_short_arg(m) - 1;
+    }
+    else
+    {
+      m->pc += 2;
+    }
+    break;
+  }
+  case OP_LDC_W:
+  {
+    push(m, get_constant(m, get_short_arg(m)));
+    m->pc += 2;
+    break;
+  }
+  case OP_ILOAD:
+  {
+    m->pc++;
+    push(m, get_local_variable(m, get_instruction(m)));
+    break;
+  }
+  case OP_ISTORE:
+  {
+    m->pc++;
+    m->st->data[get_instruction(m)] = pop(m);
+    break;
+  }
+  case OP_IINC:
+  {
+    m->pc++;
+    byte_t index = get_instruction(m);
+    m->pc++;
+    m->st->data[index] += get_instruction(m);
+    break;
+  }
+  case OP_WIDE:
+  {
+    m->pc++;
+    byte_t next_instruction = get_instruction(m);
+    m->pc++;
+    uint16_t index = (uint16_t)((uint16_t)get_instruction(m) << 8) | (uint16_t)get_text(m)[m->pc + 1];
+    m->pc++;
+    switch (next_instruction)
+    {
+    case OP_ILOAD:
+      push(m, get_local_variable(m, index));
+      break;
+    case OP_ISTORE:
+    {
+      m->st->data[index] = pop(m);
       break;
     }
-    case OP_ILOAD: {
-      m->pc++;
-      push(m, get_local_variable(m, get_instruction(m)));
-      break;
-    }
-    case OP_ISTORE: {
-      m->pc++;
-      m->st->data[get_instruction(m)] = pop(m);
-      break;
-    }
-    case OP_IINC: {
-      m->pc++;
-      byte_t index = get_instruction(m);
+    case OP_IINC:
+    {
       m->pc++;
       m->st->data[index] += get_instruction(m);
       break;
     }
-    case OP_WIDE: {
-      m->pc++;
-      byte_t next_instruction = get_instruction(m);
-      m->pc++;
-      uint16_t index = (uint16_t)((uint16_t)get_instruction(m) << 8) | (uint16_t)get_text(m)[m->pc+1];
-      m->pc++;
-      switch (next_instruction)
-      {
-        case OP_ILOAD:
-          push(m, get_local_variable(m,index));
-          break;
-        case OP_ISTORE: {
-          m->st->data[index] = pop(m);
-          break;
-        }
-        case OP_IINC: {
-          m->pc++;
-          m->st->data[index] += get_instruction(m);
-          break;
-        }
-      }
-      break;
     }
+    break;
+  }
   }
   m->pc++;
 }
 
-byte_t get_instruction(ijvm* m) 
-{ 
-  return get_text(m)[get_program_counter(m)]; 
+byte_t get_instruction(ijvm *m)
+{
+  return get_text(m)[get_program_counter(m)];
 }
 
-ijvm* init_ijvm_std(char *binary_path) 
+ijvm *init_ijvm_std(char *binary_path)
 {
   return init_ijvm(binary_path, stdin, stdout);
 }
 
-void run(ijvm* m) 
+void run(ijvm *m)
 {
-  while (!finished(m)) 
+  while (!finished(m))
   {
     step(m);
   }
 }
 
-
 // Below: methods needed by bonus assignments, see ijvm.h
-// You can leave these unimplemented if you are not doing these bonus 
+// You can leave these unimplemented if you are not doing these bonus
 // assignments.
 
-int get_call_stack_size(ijvm* m) 
+int get_call_stack_size(ijvm *m)
 {
-   // TODO: implement me if doing tail call bonus
-   return 0;
+  // TODO: implement me if doing tail call bonus
+  return 0;
 }
 
-
-// Checks if reference is a freed heap array. Note that this assumes that 
-// 
-bool is_heap_freed(ijvm* m, word_t reference) 
+// Checks if reference is a freed heap array. Note that this assumes that
+//
+bool is_heap_freed(ijvm *m, word_t reference)
 {
-   // TODO: implement me if doing garbage collection bonus
-   return 0;
+  // TODO: implement me if doing garbage collection bonus
+  return 0;
 }
